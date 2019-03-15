1280653798cb57de4763cf27ebd26b4e6b54fac8
yttrium7 <yttrium.l@pm.me>
2019-03-15 14:49:23 +0800

    add app

---
+++ mapHelper/app/build/generated/source/apt/debug/com/example/vulpix/maphelper/controller/activity/SelfNavActivity_ViewBinding.java
@@ -0,0 +1,38 @@
+// Generated code from Butter Knife. Do not modify!
+package com.example.vulpix.maphelper.controller.activity;
+
+import android.support.annotation.CallSuper;
+import android.support.annotation.UiThread;
+import android.view.View;
+import butterknife.Unbinder;
+import butterknife.internal.Utils;
+import com.example.vulpix.maphelper.R;
+import com.mapbox.mapboxsdk.maps.MapView;
+import java.lang.IllegalStateException;
+import java.lang.Override;
+
+public class SelfNavActivity_ViewBinding implements Unbinder {
+  private SelfNavActivity target;
+
+  @UiThread
+  public SelfNavActivity_ViewBinding(SelfNavActivity target) {
+    this(target, target.getWindow().getDecorView());
+  }
+
+  @UiThread
+  public SelfNavActivity_ViewBinding(SelfNavActivity target, View source) {
+    this.target = target;
+
+    target.mapView = Utils.findRequiredViewAsType(source, R.id.map_view, "field 'mapView'", MapView.class);
+  }
+
+  @Override
+  @CallSuper
+  public void unbind() {
+    SelfNavActivity target = this.target;
+    if (target == null) throw new IllegalStateException("Bindings already cleared.");
+    this.target = null;
+
+    target.mapView = null;
+  }
+}
